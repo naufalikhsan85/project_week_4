e9a16b2a5e7d26cdbed9881870ffc3d988468e84
naufal.ikhsan <naufal.ikhsan@dattabot.io>
2023-09-09 11:51:22 +0700

    initial first js

---
+++ test1.js
@@ -0,0 +1,10 @@
+let a = "saya punya"
+let b = "laptop"
+let num_laptop = 1
+
+function main(){
+    const c = a + " " + num_laptop + " " + b
+    const d = 100 + num_laptop
+    return d
+}
+console.log(main())
